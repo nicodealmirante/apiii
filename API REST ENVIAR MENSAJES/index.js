338e80fec1e0e5381b01babbe23dc355cd3268d3
nicodealmirante <nicolas.e.luna@gmail.com>
2024-05-06 10:04:31 -0300

    dfdg

--- API REST ENVIAR MENSAJES/index.js
+++ API REST ENVIAR MENSAJES/index.js
@@ -2,7 +2,6 @@ const qrcode = require('qrcode-terminal');
 const { Client } = require('whatsapp-web.js');
 const express = require('express');
 const bodyParser = require('body-parser');
-const qrcodeImage = require('qrcode');
 const fs = require('fs');
 
 //PRIMERO LE DAN npm i 
@@ -11,54 +10,36 @@ const fs = require('fs');
 
 
 
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 const client = new Client
 app.post('/enviar-mensaje', async (req, res) => {
   const { numero, mensaje } = req.body;
 
   if (!numero || !mensaje) {
-    res.status(400).json({ error: 'El número y el mensaje son requeridos.' });
-    return;
+    res.status(400).json({ error: 'El número y el mensaje son requeridos.' })
+    return
   }
 
   try {
-    await client.sendMessage(`${numero}@c.us`, mensaje);
-    res.json({ message: `Mensaje enviado a ${numero}: ${mensaje}` });
+    await client.sendMessage(`${numero}@c.us`, mensaje)
+    res.json({ message: `Mensaje enviado a ${numero}: ${mensaje}` })
   } catch (error) {
-    res.status(500).json({ error: 'Ocurrió un error al enviar el mensaje.' });
+    res.status(500).json({ error: 'Ocurrió un error al enviar el mensaje.' })
   }
-});
+})
 
 client.on('qr', (qr) => {
-  console.log('QR recibido:');
-  qrcode.generate(qr, { small: true });
+  console.log('QR recibido:')
+  qrcode.generate(qr, { small: true })
 
-  // Generar imagen con el código QR
-  qrcodeImage.toFile(
-    './qr.png',
-    qr,
-    { errorCorrectionLevel: 'H' },
-    (err) => {
-      if (err) throw err;
-      console.log('Código QR generado en qr.png');
-    }
-  );
-});
-
-client.on('ready', () => {
-  console.log('Cliente de WhatsApp listo!');
-});
-
-client.initialize();
-
-app.get('/qr', (req, res) => {
-  // Leer archivo de imagen y enviarlo como respuesta
+  client.on('ready', () => {
+  console.log('Cliente de WhatsApp listo!')
   
-});
+client.initialize()
 
 app.listen(9000, () => {
-  console.log('API REST de WhatsApp iniciada en el puerto 9000.');
-});
+  console.log('API REST de WhatsApp iniciada en el puerto 9000.')}
+)})})
